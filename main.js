9b17d9859bd1a2c2b01b37e328fbd13fd2eec117
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2021-07-03 10:50:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function angler(){
+window.location = "angler.html";
+}
+function tardigrade(){
+window.location = "tardi.html";
+}
+function search(){
+animal_name = document.getElementById("animal_search").value;
+document.getElementById("changer").innerHTML = animal_name;
+if(animal_name == "Anglerfish"){
+window.location = "angler.html";
+}
+if(animal_name == "Angler Fish"){
+window.location = "angler.html";
+}
+if(animal_name == "Tardigrade"){
+window.location = "tardi.html";
+}
+if(animal_name == "Thorny Devil"){
+window.loaction = "thorny.html";
+}
+if(animal_name == "Tamarin"){
+window.loaction = "tamarin.html";
+}
+}
+function lister(){
+window.location = "list.html";
+}
+function home(){
+window.location = "index.html";
+}
+function thorny(){
+window.location = "thorny.html"
+}
+function tamarin(){
+window.loaction = "tamarin.html";
+}
