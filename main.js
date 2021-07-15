9bb646ce37eb5d8645c4c50e97802a1ae934f810
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2021-07-15 15:26:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,23 +5,412 @@ function tardigrade(){
 window.location = "tardi.html";
 }
 function search(){
-animal_name = document.getElementById("animal_search").value;
-document.getElementById("changer").innerHTML = animal_name;
+  animal_name = document.getElementById("animal_search").value;
 if(animal_name == "Anglerfish"){
 window.location = "angler.html";
 }
 if(animal_name == "Angler Fish"){
 window.location = "angler.html";
 }
+if(animal_name == "ANGLERFISH"){
+window.location = "angler.html";
+}
+if(animal_name == "ANGLER FISH"){
+window.location = "angler.html";
+}
+if(animal_name == "anglerfish"){
+window.location = "angler.html";
+}
+if(animal_name == "angler fish"){
+  window.location = "angler.html";
+  }
 if(animal_name == "Tardigrade"){
 window.location = "tardi.html";
 }
+if(animal_name == "TARDIGRADE"){
+  window.location = "tardi.html";
+  }
+  if(animal_name == "tardigrade"){
+    window.location = "tardi.html";
+    }
 if(animal_name == "Thorny Devil"){
 window.loaction = "thorny.html";
 }
+if(animal_name == "THORNY DEVIL"){
+  window.loaction = "thorny.html";
+  }
+  if(animal_name == "thorny devil"){
+    window.loaction = "thorny.html";
+    }
 if(animal_name == "Tamarin"){
-window.loaction = "tamarin.html";
+window.location = "tamarin.html";
 }
+if(animal_name == "TAMARIN"){
+  window.location = "tamarin.html";
+  }
+  if(animal_name == "tamarin"){
+    window.location = "tamarin.html";
+    }
+if(animal_name == "Mega Piranha"){
+window.location = "mega.html";
+}
+if(animal_name == "mega piranha"){
+  window.location = "mega.html";
+  }
+  if(animal_name == "MEGA PIRANHA"){
+    window.location = "mega.html";
+    }
+if(animal_name == "Megapiranha"){
+window.location = "mega.html";
+}
+if(animal_name == "MEGAPIRANHA"){
+  window.location = "mega.html";
+  }
+  if(animal_name == "megapiranha"){
+    window.location = "mega.html";
+    }
+if(animal_name == "Venezuelan Poodle Moth"){
+window.location = "pmoth.html";
+}
+if(animal_name == "VENEZUELAN POODLE MOTH"){
+  window.location = "pmoth.html";
+  }
+  if(animal_name == "venezuelan poodle moth"){
+    window.location = "pmoth.html";
+    }
+if(animal_name == "Venezuelan Poodlemoth"){
+window.location = "pmoth.html";
+}
+if(animal_name == "VENEZUELAN POODLEMOTH"){
+  window.location = "pmoth.html";
+  }
+  if(animal_name == "venezuelan poodlemoth"){
+    window.location = "pmoth.html";
+    }
+if(animal_name == "Venezuelan PoodleMoth"){
+window.location = "pmoth.html";
+}
+if(animal_name == "Patagonian Mara"){
+window.location = "mara.html";
+}
+if(animal_name == "PATAGONIAN MARA"){
+  window.location = "mara.html";
+  }
+  if(animal_name == "patagonian mara"){
+    window.location = "mara.html";
+    }
+if(animal_name == "Patagonianmara"){
+window.location = "mara.html";
+}
+if(animal_name == "PATAGONIANMARA"){
+  window.location = "mara.html";
+  }
+  if(animal_name == "patagonianmara"){
+    window.location = "mara.html";
+    }
+if(animal_name == "PatagonianMara"){
+window.location = "mara.html";
+}
+if(animal_name == "Patagonian mara"){
+window.location = "mara.html";
+}
+if(animal_name == "patagonian Mara"){
+  window.location = "mara.html";
+}
+if(animal_name == "Rhinopithecus"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "RHINOPITHECUS"){
+  window.location = "rhinophc.html";
+  }
+  if(animal_name == "rhinopithecus"){
+    window.location = "rhinophc.html";
+    }
+if(animal_name == "Snub-Nosed Monkey"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "SNUB-NOSED MONKEY"){
+  window.location = "rhinophc.html";
+  }
+  if(animal_name == "snub-nosed monkey"){
+    window.location = "rhinophc.html";
+    }
+if(animal_name == "Snub Nosed Monkey"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "SNUB NOSED MONKEY"){
+  window.location = "rhinophc.html";
+  }
+  if(animal_name == "snub nosed monkey"){
+    window.location = "rhinophc.html";
+    }
+if(animal_name == "SnubNosed Monkey"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "snubnosed monley"){
+  window.location = "rhinophc.html";
+  }
+  if(animal_name == "SNUBNOSED MONKEY"){
+    window.location = "rhinophc.html";
+    }
+if(animal_name == "Snub-nosed monkey"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "Snubnosed Monkey"){
+window.location = "rhinophc.html";
+}
+if(animal_name == "Tarsier"){
+window.location = "tarsier.html";
+}
+if(animal_name == "TARSIER"){
+  window.location = "tarsier.html";
+  }
+  if(animal_name == "tarsier"){
+    window.location = "tarsier.html";
+    }
+if(animal_name == "Electric Eel"){
+window.location = "eel.html";
+}
+if(animal_name == "ELECTRIC EEL"){
+  window.location = "eel.html";
+  }
+  if(animal_name == "electric eel"){
+    window.location = "eel.html";
+    }
+    if(animal_name == "Electric eel"){
+      window.location = "eel.html";
+      }
+if(animal_name == "Electriceel"){
+window.location = "eel.html";
+}
+if(animal_name == "ELECTRICEEL"){
+  window.location = "eel.html";
+  }
+  if(animal_name == "electriceel"){
+    window.location = "eel.html";
+    }
+if(animal_name == "Capybara"){
+window.location = "capybara.html";
+}
+if(animal_name == "capybara"){
+  window.location = "capybara.html";
+  }
+  if(animal_name == "CAPYBARA"){
+    window.location = "capybara.html";
+    }
+if(animal_name == "Capy Bara"){
+window.location = "capybara.html";
+}
+if(animal_name == "capy bara"){
+  window.location = "capybara.html";
+  }
+  if(animal_name == "CAPY BARA"){
+    window.location = "capybara.html";
+    }
+if(animal_name == "CapyBara"){
+window.location = "capybara.html"
+}
+if(animal_name == "Mangalica"){
+window.location = "mangalica.html";
+}
+if(animal_name == "mangalica"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "MANGALICA"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Mangalica Pig"){
+window.location = "mangalica.html";
+}
+if(animal_name == "mangalica pig"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "MANGALICA PIG"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Mangalitsa"){
+window.location = "mangalica.html";
+}
+if(animal_name == "MANGALITSA"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "mangalitsa"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Mangalitsa Pig"){
+window.location = "mangalica.html";
+}
+if(animal_name == "mangalitsa pig"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "MANGALITSA PIG"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Mangalitza"){
+window.location = "mangalica.html";
+}
+if(animal_name == "MANGALITZA"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "mangalitza"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Mangalitza Pig"){
+window.location = "mangalica.html";
+}
+if(animal_name == "mangalitza pig"){
+  window.location = "mangalica.html";
+  }
+  if(animal_name == "MANGALITZA PIG"){
+    window.location = "mangalica.html";
+    }
+if(animal_name == "Sunda Flying Lemur"){
+window.location = "colugo.html";
+}
+if(animal_name == "sunda flying lemur"){
+  window.location = "colugo.html";
+  }
+  if(animal_name == "SUNDA FLYING LEMUR"){
+    window.location = "colugo.html";
+    }
+if(animal_name == "Sunda Colugo"){
+window.location = "colugo.html";
+}
+if(animal_name == "sunda colugo"){
+  window.location = "colugo.html";
+  }
+  if(animal_name == "SUNDA COLUGO"){
+    window.location = "colugo.html";
+    }
+if(animal_name == "Malayan Flying Lemur"){
+window.location = "colugo.html";
+}
+if(animal_name == "MALAYAN FLYING LEMUR"){
+  window.location = "colugo.html";
+  }
+  if(animal_name == "malayan flying lemur"){
+    window.location = "colugo.html";
+    }
+if(animal_name == "Malayan Colugo"){
+window.location = "colugo.html";
+}
+if(animal_name == "malayan colugo"){
+  window.location = "colugo.html";
+  }
+  if(animal_name == "MALAYAN COLUGO"){
+    window.location = "colugo.html";
+    }
+if(animal_name == "Okapi"){
+window.location = "okapi.html";
+}
+if(animal_name == "okapi"){
+  window.location = "okapi.html";
+  }
+  if(animal_name == "OKAPI"){
+    window.location = "okapi.html";
+    }
+if(animal_name == "Zebra Duiker"){
+window.location = "duiker.html";
+}
+if(animal_name == "zebra duiker"){
+  window.location = "duiker.html";
+  }
+  if(animal_name == "ZEBRA DUIKER"){
+    window.location = "duiker.html";
+    }
+if(animal_name == "Glaucus Atlanticus"){
+window.location = "dragon.html";
+}
+if(animal_name == "glaucus atlanticus"){
+  window.location = "dragon.html";
+  }
+  if(animal_name == "GLAUCUS ATLANTICUS"){
+    window.location = "dragon.html";
+    }
+if(animal_name == "Blue Dragon"){
+window.location = "dragon.html";
+}
+if(animal_name == "blue dragon"){
+  window.location = "dragon.html";
+  }
+  if(animal_name == "BLUE DRAGON"){
+    window.location = "dragon.html";
+    }
+if(animal_name == "Lophorina"){
+window.location = "lophorina.html";
+}
+if(animal_name == "lophorina"){
+  window.location = "lophorina.html";
+  }
+  if(animal_name == "LOPHORINA"){
+    window.location = "lophorina.html";
+    }
+if(animal_name == "Tufted Deer"){
+window.location = "tufted.html";
+}
+if(animal_name == "tufted deer"){
+  window.location = "tufted.html";
+  }
+  if(animal_name == "TUFTED DEER"){
+    window.location = "tufted.html";
+    }
+if(animal_name == "Red-Lipped Batfish"){
+window.location = "batfish.html";
+}
+if(animal_name == "RED-LIPPED BATFISH"){
+  window.location = "batfish.html";
+  }
+  if(animal_name == "red-lipped batfish"){
+    window.location = "batfish.html";
+    }
+if(animal_name == "Red Lipped Batfish"){
+window.location = "batfish.html";
+}
+if(animal_name == "red lipped batfish"){
+  window.location = "batfish.html";
+  }
+  if(animal_name == "RED LIPPED BATFISH"){
+    window.location = "batfish.html";
+    }
+if(animal_name == "Redlipped Batfish"){
+window.location = "batfish.html";
+}
+if(animal_name == "redlipped batfish"){
+  window.location = "batfish.html";
+  }
+  if(animal_name == "REDLIPPED BATFISH"){
+    window.location = "batfish.html";
+    }
+if(animal_name == "RedLipped Batfish"){
+window.location = "batfish.html";
+}
+if(animal_name == "Galapagos Batfish"){
+window.location = "batfish.html";
+}
+if(animal_name == "galapagos batfish"){
+  window.location = "batfish.html";
+  }
+  if(animal_name == "GALAPAGOS BATFISH"){
+    window.location = "batfish.html";
+    }
+if(animal_name == "Panda Ant"){
+window.location = "p_ant.html";
+}
+if(animal_name == "PANDA ANT"){
+  window.location = "p_ant.html";
+  }
+  if(animal_name == "panda ant"){
+    window.location = "p_ant.html";
+    }
+if(animal_name == "Euspaniola Militaris"){
+window.location = "p_ant.html";
+}
+if(animal_name == "euspaniola militaris"){
+  window.location = "p_ant.html";
+  }
+  if(animal_name == "EUSPANIOLA MILITARIS"){
+    window.location = "p_ant.html";
+    }
 }
 function lister(){
 window.location = "list.html";
@@ -33,5 +422,53 @@ function thorny(){
 window.location = "thorny.html"
 }
 function tamarin(){
-window.loaction = "tamarin.html";
+window.location = "tamarin.html";
+}
+function mega(){
+window.location = "mega.html";
+}
+function vpmoth(){
+window.location = "pmoth.html";
+}
+function mara(){
+window.location = "mara.html";
+}
+function snm(){
+window.location = "rhinophc.html";
+}
+function tarsier(){
+window.location = "tarsier.html";
+}
+function eel(){
+window.location = "eel.html";
+}
+function capybara(){
+window.location = "capybara.html";
+}
+function mangalica(){
+window.location = "mangalica.html";
+}
+function colugo(){
+window.location = "colugo.html";
+}
+function duiker(){
+window.location = "duiker.html";
+}
+function okapi(){
+window.location = "okapi.html";
+}
+function dragon(){
+window.location = "dragon.html";
+}
+function lophorina(){
+window.location = "lophorina.html";
+}
+function tufted(){
+window.location = "tufted.html";
+}
+function batfish(){
+window.location = "batfish.html";
+}
+function pant(){
+window.location = "p_ant.html";
 }
