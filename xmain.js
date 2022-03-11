58779ad6c4972c2995cd3cfae4ed997aef325b3e
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2022-03-11 17:17:57 +0530

    Add files via upload

---
+++ xmain.js
@@ -0,0 +1,159 @@
+function c_home(){
+window.location = "xindex.html";
+}
+function c_lister(){
+window.location = "listx.html";
+}
+function c_search(){
+c_a_name = document.getElementById("c_animal_search").value;
+if(c_a_name == "Goose"){
+window.location = "goose.html";
+}
+if(c_a_name == "Mouse"){
+window.location = "mouse.html";
+}
+if(c_a_name == "Cat"){
+window.location = "cat.html";
+}
+if(c_a_name == "Dog"){
+window.location = "dog.html";
+}
+if(c_a_name == "Pig"){
+window.location = "pig.html";
+}
+if(c_a_name == "Duck"){
+window.location = "duck.html";
+}
+if(c_a_name == "Moose"){
+window.location = "moose.html";
+}
+if(c_a_name == "Donkey"){
+window.location = "donkey.html";
+}
+if(c_a_name == "Ostrich"){
+window.location = "ostrich.html";
+}
+if(c_a_name == "Squirrel"){
+window.location = "squirrel.html";
+}
+if(c_a_name == "Crow"){
+window.location = "crow.html";
+}
+if(c_a_name == "Camel"){
+window.location = "camel.html";
+}
+if(c_a_name == "Bear"){
+window.location = "bear.html";
+}
+if(c_a_name == "Raven"){
+window.location = "raven.html";
+}
+if(c_a_name == "Pigeon"){
+window.location = "pigeon.html";
+}
+if(c_a_name == "Chicken"){
+window.location = "chicken.html";
+}
+if(c_a_name == "Eagle"){
+window.location = "eagle.html";
+}
+if(c_a_name == "Jackal"){
+window.location = "jackal.html";
+}
+if(c_a_name == "Fox"){
+window.location = "fox.html";
+}
+if(c_a_name == "Zebra"){
+window.location = "zebra.html";
+}
+if(c_a_name == "Crab"){
+window.location = "crab.html";
+}
+if(c_a_name == "Snail"){
+window.location = "snail.html";
+}
+if(c_a_name == "Tortoise"){
+window.location = "tortoise.html";
+}
+if(c_a_name == "Hippopotamus"){
+    window.location = "hippo.html";
+}
+if(c_a_name == "Gorilla"){
+    window.location = "gorilla.html";
+}
+}
+function goose(){
+window.location = "goose.html"
+}
+function mouse(){
+window.location = "mouse.html";
+}
+function cat(){
+window.location = "cat.html";
+}
+function dog(){
+window.location = "dog.html";
+}
+function pig(){
+window.location = "pig.html";
+}
+function duck(){
+window.location = "duck.html";
+}
+function moose(){
+window.location = "moose.html";
+}
+function donkey(){
+window.location = "donkey.html";
+}
+function ostrich(){
+window.location = "ostrich.html";
+}
+function squirrel(){
+window.location = "squirrel.html";
+}
+function crow(){
+window.location = "crow.html";
+}
+function camel(){
+window.location = "camel.html";
+}
+function bear(){
+window.location = "bear.html";
+}
+function raven(){
+window.location = "raven.html";
+}
+function pigeon(){
+window.location = "pigeon.html";
+}
+function chicken(){
+window.location = "chicken.html";
+}
+function eagle(){
+window.location = "eagle.html";
+}
+function jackal(){
+window.location = "jackal.html";
+}
+function fox(){
+window.location = "fox.html";
+}
+function zebra(){
+window.location = "zebra.html";
+}
+function crab(){
+window.location = "crab.html";
+}
+function snail(){
+window.location = "snail.html";
+}
+function tortoise(){
+window.location = "tortoise.html";
+}
+function hippo(){
+window.location = "hippo.html";
+}
+function gorilla(){
+window.location = "gorilla.html";
+}
